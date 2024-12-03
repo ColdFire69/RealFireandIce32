e02f93ca762bb4e066a285c15890af19374ebec4
RealZ <mamaadriel89@gmail.com>
2024-12-03 18:21:39 +0000

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,14 @@
+print("Welcome to Python!")
+#The code above writes the words: Welcome to Python!
+creator = "Adriel"
+food="noodles"
+#This is a basic showcase of how variables work (above).
+numbers = [1, 2, 3, 4, 5]
+#Here's a list(above)!
+#Credits to Gamma for these 2 lines of code (below).
+name = input("What's your name?")
+print(f"Hello {name}!")
+#Above and below, is a question. Use this to make your on custom Q and A!
+country = input("Which country are you in at the moment?")
+print(f"So you are in {country}? Cool!")
+
